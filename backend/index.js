5f9d1fd532b3240ec3214d4ad0f573678c754068
bhupendra yadav <54168804+bhupendra8753@users.noreply.github.com>
2020-02-07 11:50:27 +0530

    my-commit

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 const http = require("http");
-const debug = require("debug")("node-angular");
 
 
 //load routes
